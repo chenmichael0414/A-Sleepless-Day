4b8be33ad3042f683ffd097b02544cfe6a37f428
Preston <55302754+prestonmeek@users.noreply.github.com>
2022-03-23 10:48:39 -0400

    Incorporated the other animations made by Michael, added wall collisions

--- screen.py
+++ screen.py
@@ -1,7 +1,12 @@
 import pygame
 
 class Screen:
-    def __init__(self, width=800, height=800, fps=60):
+    def __init__(self, width=800, height=600, fps=60):
+        self.PIXEL_SIZE       = 32
+        self.PLAYER_SCALE     = 2.25
+        self.BACKGROUND_SCALE = 15
+        self.BACKGROUND_SIZE  = self.PIXEL_SIZE * self.PLAYER_SCALE * self.BACKGROUND_SCALE * 2
+
         self.SCREEN_WIDTH  = width
         self.SCREEN_HEIGHT = height
         self.display = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
@@ -10,7 +15,7 @@ class Screen:
         self.fps = fps
 
         self.bg = pygame.image.load("background.jpeg")
-        self.bg = pygame.transform.scale(self.bg, (self.SCREEN_WIDTH * 3, self.SCREEN_HEIGHT * 3))
+        self.bg = pygame.transform.scale(self.bg, (self.BACKGROUND_SIZE, self.BACKGROUND_SIZE))
 
         self.frame = 0
 
