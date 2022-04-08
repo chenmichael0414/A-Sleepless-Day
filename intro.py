97a7dfd932ec3ba77420b3832e597b4d9c108757
BevinTheBeverage <38870147+BevinTheBeverage@users.noreply.github.com>
2022-04-08 18:13:08 -0400

    separated out backgrounds and started coding intro

---
+++ intro.py
@@ -0,0 +1,9 @@
+from player import Player
+from screen import Screen
+from textbox import Textbox
+from item import Item
+from inventory import Inventory
+from key import Key
+
+def cutscene(screen):
+    screen.setRoom('NEIGHBORHOOD')
