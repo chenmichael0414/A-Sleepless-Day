ada02037e73463f03a1e4e5f7ef4bab88080bc1d
chenmichael0414 <chenmichael0414@gmail.com>
2022-05-05 21:27:51 -0400

    finished mole

--- boss_battles/mole.py
+++ boss_battles/mole.py
@@ -6,6 +6,8 @@ from colliders import ImageCollider
 class Mole(Boss):
     def __init__(self, screen, battle, textbox):
         self.count = 0
+        self.change = 0
+        self.yMemory = 0
         super().__init__(
             screen, 
             battle, 
@@ -13,13 +15,15 @@ class Mole(Boss):
             './enemies/bosses/mole.png',
             519,
             [
-                self.bolts
+                self.tornado,
+                self.bolts,
+                self.tornadoBolts
             ],
             {
                 'bolt1': ImageCollider((0, 0), pygame.image.load('./attacks/bolt1.png').convert_alpha()),
                 'bolt2': ImageCollider((0, 0), pygame.image.load('./attacks/bolt2.png').convert_alpha()),
                 'bolt3': ImageCollider((0, 0), pygame.image.load('./attacks/bolt3.png').convert_alpha()),
-                'ballFire': ImageCollider((0, 0), pygame.image.load('./attacks/ballFire.png').convert_alpha())
+                'tornado': ImageCollider((0, 0), pygame.image.load('./attacks/tornado.png').convert_alpha())
             }
         )
 
@@ -29,7 +33,8 @@ class Mole(Boss):
 
         self.drawBoss()
 
-        if self.textbox.drawIfIncomplete(['I am the fastest mole alive!', 'prepare to be electrified!'], 'mole intro'): 
+        if self.textbox.drawIfIncomplete(['I am the fastest mole alive!', 'Feel the wind beneath my feet!'], 'mole intro'): 
+            self.battle.mode = "FREE MOVE"
             return
 
         for minion in self.minions:
@@ -42,6 +47,43 @@ class Mole(Boss):
         super().reset()
 
         self.textbox.resetFlag('mole intro')
+
+    def tornado(self):
+        for minion in self.minions:
+            xSpeed = random.randint(0,4)
+            xSpeed*=3
+            if self.change%60==0:
+                ySpeed = random.randint(-5,5)
+                self.yMemory = ySpeed
+            else:
+                ySpeed = self.yMemory
+
+            self.change+=1
+
+            minion['x'] += xSpeed
+            minion['y'] += ySpeed
+
+        if self.defeatedMinions < 30:
+            topQuarter    = (2*self.screen.SCREEN_HEIGHT - self.battle.boxHeight) / 4
+            bottomQuarter = (2*self.screen.SCREEN_HEIGHT + self.battle.boxHeight) / 4
+            # If it is the current frame to draw a minion
+            if len(self.minions) < 10 and self.screen.frame % 35 == 0:
+                self.minions.append({
+                        'x': 0,
+                        'y': random.randint(int(topQuarter), int(bottomQuarter)),
+                        'dir': 1,
+                        'sprite': pygame.image.load('./attacks/tornado.png').convert_alpha(),
+                        'type': 'tornado'
+                })
+        # If we have defeated enough minions to proceed and all of the minions have despawned, proceed
+        elif len(self.minions) == 0:
+            self.loadSprite(1)
+
+            if self.textbox.drawIfIncomplete(['Now face the wrath of my lightning!!'], 'mole tornado win'): return
+
+            self.defeatedMinions = 0
+            self.currentAttack += 1
+
 
     def bolts(self):
         for minion in self.minions:
@@ -64,15 +106,14 @@ class Mole(Boss):
                 minion['x'] += xSpeed
                 minion['y'] += ySpeed
 
-        if self.defeatedMinions < 40:
+        if self.defeatedMinions < 30:
             # If it is the current frame to draw a minion
-            if len(self.minions) < 10 and self.screen.frame % 30 == 0:
+            if len(self.minions) < 10 and self.screen.frame % 45 == 0:
                 self.count+=1
                 if self.count%2 == 0:
                     topBoxCorner    = (self.screen.SCREEN_HEIGHT - self.battle.boxHeight) / 2
                     leftBoxCorner  = (self.screen.SCREEN_WIDTH - self.battle.boxWidth) / 2
                     yValue = random.randint(topBoxCorner, 2*topBoxCorner)
-                    print(yValue)
                     self.minions.append({
                         'x': leftBoxCorner,
                         'y': yValue,
@@ -105,11 +146,106 @@ class Mole(Boss):
                         'sprite': pygame.image.load('./attacks/bolt2.png').convert_alpha(),
                         'type': 'bolt2'
                     })
+            # If we have defeated enough minions to proceed and all of the minions have despawned, proceed
+        elif len(self.minions) == 0:
+            self.loadSprite(2)
+
+            if self.textbox.drawIfIncomplete(['Now I\'m angry!'], 'mole bolt win'): return
+
+            self.defeatedMinions = 0
+            self.currentAttack += 1
                     
+    def tornadoBolts(self):
+        for minion in self.minions:
+            if minion['type'] == 'bolt1':
+                xSpeed = -4
+                ySpeed = 4
+
+                minion['x'] += xSpeed
+                minion['y'] += ySpeed
+            elif minion['type'] == 'bolt2':
+                xSpeed = 0
+                ySpeed = 7
+
+                minion['x'] += xSpeed
+                minion['y'] += ySpeed
+            elif minion['type'] == 'bolt3':
+                xSpeed = 4
+                ySpeed = 4
+
+                minion['x'] += xSpeed
+                minion['y'] += ySpeed
+            elif minion['type'] == 'tornado':
+                xSpeed = random.randint(0,4)
+                xSpeed*=3
+                if self.change%30==0:
+                    ySpeed = random.randint(-5,5)
+                    self.yMemory = ySpeed
+                else:
+                    ySpeed = self.yMemory
+
+                self.change+=1
+                minion['x'] += xSpeed
+                minion['y'] += ySpeed
+        rng = random.randint(0,4)
+        if self.defeatedMinions < 45:
+            # If it is the current frame to draw a minion
+            if len(self.minions) < 10 and self.screen.frame % 35 == 0:
+                if rng < 2:
+                    topBoxCorner    = (self.screen.SCREEN_HEIGHT - self.battle.boxHeight) / 2
+                    leftBoxCorner  = (self.screen.SCREEN_WIDTH - self.battle.boxWidth) / 2
+                    yValue = random.randint(topBoxCorner, 2*topBoxCorner)
+                    self.minions.append({
+                        'x': leftBoxCorner,
+                        'y': yValue,
+                        'dir': 1,
+                        'sprite': pygame.image.load('./attacks/bolt3.png').convert_alpha(),
+                        'type': 'bolt3'
+                    })
+                    rightBoxCorner = (self.screen.SCREEN_WIDTH + self.battle.boxWidth) / 2
+                    self.minions.append({
+                        'x': rightBoxCorner - 50,
+                        'y': yValue,
+                        'dir': 1,
+                        'sprite': pygame.image.load('./attacks/bolt1.png').convert_alpha(),
+                        'type': 'bolt1'
+                    })
+                elif rng < 4:
+                    leftBoxCorner  = (self.screen.SCREEN_WIDTH - self.battle.boxWidth) / 2
+                    rightBoxCorner = (self.screen.SCREEN_WIDTH + self.battle.boxWidth) / 2
+                    self.minions.append({
+                        'x': random.randint(leftBoxCorner, rightBoxCorner - 50),
+                        'y': 0,
+                        'dir': 1,
+                        'sprite': pygame.image.load('./attacks/bolt2.png').convert_alpha(),
+                        'type': 'bolt2'
+                    })
+                    self.minions.append({
+                        'x': random.randint(leftBoxCorner, rightBoxCorner - 50),
+                        'y': 0,
+                        'dir': 1,
+                        'sprite': pygame.image.load('./attacks/bolt2.png').convert_alpha(),
+                        'type': 'bolt2'
+                    })
+                else:
+                    topQuarter    = (2*self.screen.SCREEN_HEIGHT - self.battle.boxHeight) / 4
+                    bottomQuarter = (2*self.screen.SCREEN_HEIGHT + self.battle.boxHeight) / 4
+                    # If it is the current frame to draw a minion
+                    if len(self.minions) < 10 and self.screen.frame % 35 == 0:
+                        self.minions.append({
+                                'x': 0,
+                                'y': random.randint(int(topQuarter), int(bottomQuarter)),
+                                'dir': 1,
+                                'sprite': pygame.image.load('./attacks/tornado.png').convert_alpha(),
+                                'type': 'tornado'
+                        })
+                    
+
+
         elif len(self.minions) == 0:
             self.loadSprite(0)
             
-            if self.textbox.drawIfIncomplete(['hey u win congrats!'], 'monkey final win'): return
+            if self.textbox.drawIfIncomplete(['hey u win congrats!'], 'mole final win'): return
 
 
         
